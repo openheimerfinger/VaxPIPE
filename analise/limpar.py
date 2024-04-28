af6168d50dece171cfead04b168a5ce37a9ee19b
Pedro Henrique Marques <167574289+openheimerfinger@users.noreply.github.com>
2024-04-27 22:49:32 -0300

    Atualizei o limpar
    Envio do segundo anexo

--- analise/limpar.py
+++ analise/limpar.py
@@ -6,7 +6,6 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email import encoders
-
 def enviar_email():
     try:
         agora = datetime.datetime.now()
@@ -15,6 +14,7 @@ def enviar_email():
         nome_atual = 'Final.zip'
         novo_nome = f'Final_{data_atual}_{hora_atual}.zip'
         os.rename(nome_atual, novo_nome)
+        arquivos_intermed = "MHCintermediariesFILES.zip"
 
         with open("email.str", "r") as email_file:
             email_content = email_file.read().strip()  # Remover espaços em branco extras
@@ -44,6 +44,16 @@ def enviar_email():
         encoders.encode_base64(parte_anexo)
         parte_anexo.add_header('Content-Disposition', f"attachment; filename= {nome_anexo}")
         msg.attach(parte_anexo)
+
+        # Adicionar segundo anexo (arquivos_intermed)
+        caminho_anexo2 = arquivos_intermed
+        nome_anexo2 = os.path.basename(caminho_anexo2)
+        arquivo_anexo2 = open(caminho_anexo2, 'rb')
+        parte_anexo2 = MIMEBase('application', 'octet-stream')
+        parte_anexo2.set_payload(arquivo_anexo2.read())
+        encoders.encode_base64(parte_anexo2)
+        parte_anexo2.add_header('Content-Disposition', f"attachment; filename= {nome_anexo2}")
+        msg.attach(parte_anexo2)
 
         # Enviar e-mail
         senha = "!Gipsysiomar"
